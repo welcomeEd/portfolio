a18b70c440fd7a4070342677440395c95376319b
welcomeEd <82434197+welcomeEd@users.noreply.github.com>
2022-08-02 00:13:13 -0700

    progress

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+//home 
+router.get('/',(req,res,next) => {
+  res.render('index');
+});
+
+router.get('/skills/',(req,res,next) => {
+  res.render('skills');
+});
+
+router.get('/resume/', (req,res,next) => {
+  res.render('resume');
+});
+
+router.get('/contact/', (req,res,next) => {
+  res.render('contact');
+});
+
+router.get('/education/', (req,res,next) => {
+  res.render('education')
 });
 
 module.exports = router;
